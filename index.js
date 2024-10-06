06cbbf6e427c97f0d31f558d5bbb48db6f06120f
NureAlam68 <nurealam151068@gmail.com>
2024-10-06 22:52:50 +0600

    search text

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 
-const loadAllPhones = () => {
-    console.log('3 sec')
+const loadAllPhones = async(status, searchText) => {
+    console.log(searchText);
     document.getElementById("spinner").style.display = "none";
+
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText?searchText:"iphone"}`);
+    const data = await res.json();
+
+    console.log(data)
+
+    if (status === true) {
+        displayAllPhone(data.data)
+    }
+    else {
+        displayAllPhone(data.data.slice(0,6))
+    }
+};
+
+
+
+
+const displayAllPhone = (phones) => {
+    console.log(phones);
 }
+
+
+
+const handleShowAll = () => {
+    loadAllPhones(true)
+}
+
+
+
 
 
 
 
 const handleSearch = () => {
     document.getElementById("spinner").style.display = "block";
-    
+    const searchText = document.getElementById('search-box').value;
     setTimeout(function () {
-        loadAllPhones()
+        loadAllPhones(false, searchText)
     },3000)
-}
+};
+
+loadAllPhones(false, "iphone");
+
